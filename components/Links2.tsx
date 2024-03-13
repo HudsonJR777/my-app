38d0ac3f9af739faca72f9cbb6eb4862d564a79b
HudsonJR777 <hudsonjunior@pkkgmail.com>
2024-03-12 22:23:45 -0300

    Subindo projeto

---
+++ components/Links2.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+
+export function Links2() {
+    return (
+        <div className="absolute flex bottom-10 z-[20] right-5 flex-col md:hidden gap-5">
+        <Link
+          href="/my-skills"
+          className="rounded-[20px] group bg-blue-500 px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Learn more
+        </Link>
+
+        <Link
+          href="/my-projects"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          My projects
+        </Link>
+        <Link
+          href="/contact-me"
+          className="rounded-[20px] group bg-trasparent border border-white px-5 py-3 text-lg text-white max-w-[200px]"
+        >
+          Contact me
+        </Link>
+      </div>
+    );
+}
